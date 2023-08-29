0bd38ac2add18a45f512dd809706f03b6e8d7049
Samilly <117461633+samillyandrades@users.noreply.github.com>
2023-08-29 12:30:48 -0300

    Desenvolvendo o cadastro de livros

--- bibliodaurea/src/pages/RegisterBooks.js
+++ bibliodaurea/src/pages/RegisterBooks.js
@@ -1,17 +1,45 @@
 import styles from './css/register.module.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
 
+const Form = () => {
 
+    
+      
+
+};
 function RegisterBooks(){
     return(
-        <div>
-            <div>
-            cadastrar livros
-            </div>
+        <div className={styles.FundoRegister}>
+        
+            
             <div className={styles.FormBooks}>
-                div
+                <div className={styles.divBook}>
+                
+                </div>
 
-            </div>
+         <div className={styles.formregister}>
+            <form>
+                <label for="fname">Título do Livro:</label><br />
+                <input type="text" className={styles.Tbook} name="fname" placeholder="TÍTULO" /><br /><br></br>
+                <label for="lname">Gênero:</label><br/>
+                <input type="text" className={styles.Tbook} name="lname" /><br /><br />
+                <label for="lname">Autor:</label><br/>
+                <input type="text" className={styles.Tbook} name="lname" /><br /><br />
+                <label for="lname">Edição:</label><br/>
+                <input type="text" className={styles.Tbook} name="lname" /><br /><br />
+                <label for="lname">N° de exemplares:</label><br/>
+                <input type="text" className={styles.Tbook} name="lname" /><br /><br />
+                <label for="lname">Tipo de livro:</label><br/>
+                <input type="text" className={styles.Tbook} name="lname" /><br /><br />
+                <button className={styles.buttonForm}>Cadastrar</button>
+           </form>
+      </div>
+              
+                 </div>
+        
+   
+            
             
         </div>
         
