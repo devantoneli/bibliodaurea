5b210a8c3f6aa3891c5c50d7176a0f1616054378
Samilly <117461633+samillyandrades@users.noreply.github.com>
2023-08-29 08:28:40 -0300

    Iniciando tela de cadastro de livro

--- bibliodaurea/src/pages/ConsultBooks.js
+++ bibliodaurea/src/pages/ConsultBooks.js
@@ -1,5 +1,3 @@
-import 
-
 function ConsultBooks(){
     return(
         <div>
--- bibliodaurea/src/pages/RegisterBooks.js
+++ bibliodaurea/src/pages/RegisterBooks.js
@@ -1,9 +1,23 @@
+import styles from './css/register.module.css';
+
+
+
 function RegisterBooks(){
     return(
         <div>
+            <div>
             cadastrar livros
+            </div>
+            <div className={styles.FormBooks}>
+                div
+
+            </div>
+            
         </div>
-    )
+        
+     )
+
+    
 }
 
 export default RegisterBooks;
