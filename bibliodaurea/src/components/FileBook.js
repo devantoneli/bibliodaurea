6b2d6b760313b91a24264f1b126c1bf4c4be7829
Samilly <117461633+samillyandrades@users.noreply.github.com>
2023-09-14 12:23:31 -0300

    trabalhando no input file

--- bibliodaurea/src/components/FileBook.js
+++ bibliodaurea/src/components/FileBook.js
@@ -1,35 +1,55 @@
 import styles from '../pages/css/register.module.css';
-import photo from '../img/photo.png'
+import Event from '../components/EventClick';
 
-function FileBook(){
-    return(
 
+import React, { useRef } from 'react';
+
+function MeuComponente() {
+  const fileInputRef = useRef(null);
+
+  const handleCustomButtonClick = () => {
+    // Aciona o clique no input de arquivo quando o botão personalizado é clicado
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    const fileButton = document.getElementById('fileButton');
+
+    if (selectedFile) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (e) {
+        document.getElementById("photo").src = e.target.result;
+        let linkCapa = e.target.result;
+        console.log(linkCapa);
+        document.getElementById("linkImg").value = linkCapa;
+        // Oculta o input de arquivo após o carregamento da imagem
+        fileInputRef.current.style.display = 'none';
+      };
+      fileReader.readAsDataURL(selectedFile);
+
+      // Atualiza a visibilidade da imagem
+      document.getElementById("photo").style.opacity = '1';
+
+      console.log('Arquivo selecionado:', selectedFile.name);
+    }
+  };
+
+  return (
     <div className={styles.container}>
+      <div className={styles.max}>
+        <button className={styles.imageContainer} id="fileButton" onClick={handleCustomButtonClick}>
+            <img src="" id="photo" alt='Selecione uma imagem' className={styles.PhotoBook} />
 
-    <div className={styles.max}>
-        <div className={styles.imageContainer}>
-        <img src={photo} alt='Selecine uma imagem' className={styles.PhotoBook}></img>
+            <input id="linkImg" value="" hidden />
+
+            <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleFileChange}/>
+        </button>
+      </div>
     </div>
-
-    </div>
-     <input type="file" id='fileimage' name='fileimg' accept='image/*'/>
-
-     </div>
-
-    
-     
-        
-    )
+  );
 }
 
-let photo = document.getElementById('PhotoBook');
-let file = document.getElementById('fileimage');
-
-photo.addEventListener('click', () =>{
-    file.click();
-})
-
-
-
-export default FileBook
-
+export default MeuComponente;
