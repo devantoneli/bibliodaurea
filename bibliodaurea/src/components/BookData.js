99a5ff175245b0d6e35b4824dd83148e4ce869d2
Antoneli <antonelicavalcante@gmail.com>
2023-09-22 12:31:37 -0300

    Realizando update

--- bibliodaurea/src/components/BookData.js
+++ bibliodaurea/src/components/BookData.js
@@ -9,17 +9,19 @@ import ReactModal from 'react-modal';
 import editIcon from '../img/editIcon.png';
 import historyIcon from '../img/historyIcon.png';
 import closeIcon from '../img/closeIcon.png';
+import saveIcon from '../img/saveIcon.png';
 
 //Database
 import app from './../firebaseConfig/index.js';
-import { onValue } from 'firebase/database';
-import { getDatabase, ref, get, child } from 'firebase/database';
+import { getDatabase, ref, get, child, onValue, update } from 'firebase/database';
 
 const db = getDatabase(app);
+
 
 function BookData(props){
     const [BookInfo, setBookInfo] = useState([]);
     const [isOpen, setIsOpen] = useState(true);
+    const [isEditMode, setIsEditMode] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
@@ -40,30 +42,69 @@ function BookData(props){
         fetchData();
       }, []);
 
-      const handleClose = () => {
+    const handleClose = () => {
+        setIsOpen(false);
         props.onClose();
-      };
+    };
+
+    const handleEditClick = () => {
+        setIsEditMode(true);
+    };
 
     return (
         <div>
             {BookInfo && (
             <ReactModal className={styles.BookDataModal} isOpen={props.isOpen} onRequestClose={handleClose} contentLabel="Detalhes do livro selecionado">
-                <PopData id={BookInfo.id} title={BookInfo.title} cover={BookInfo.cover} gender={BookInfo.gender} author={BookInfo.author} name={BookInfo.status} since={BookInfo.since} copies={BookInfo.copies} quantity={BookInfo.quantity} description={BookInfo.description} registered={BookInfo.registered} type={BookInfo.type} edition={BookInfo.edition} until={BookInfo.until} onClose={() => setIsOpen(false)}/>
+                <PopData id={BookInfo.id} title={BookInfo.title} cover={BookInfo.cover} genre={BookInfo.genre} author={BookInfo.author} status={BookInfo.status} since={BookInfo.since} copies={BookInfo.copies} quantity={BookInfo.quantity} description={BookInfo.description} registered={BookInfo.registered} type={BookInfo.type} edition={BookInfo.edition} until={BookInfo.until} onClose={handleClose} isEditMode={isEditMode} onEditClick={handleEditClick}/>
             </ReactModal>)}
         </div>
         )
 
-    function PopData(props) {
+    function PopData(props) {  
+        const [editedValues, setEditedValues] = useState({
+            id: props.id,
+            title: props.title,
+            genre: props.genre,
+            description: props.description,
+            since: props.since,
+            until: props.until,
+            copies: props.copies,
+            status: props.status,
+            quantity: props.quantity,
+            registered: props.registered,
+            type: props.type,
+            edition: props.edition,
+            author: props.author,
+        });
+
+        const handleSaveClick = () => {
+            // Atualize os dados no Firebase
+            const db = getDatabase(app);
+            const bookRef = ref(db, `books/${props.id}`);
+     
+            // Use a função update para atualizar os campos específicos
+            update(bookRef, editedValues)
+            .then(() => {
+                console.log('Dados atualizados com sucesso.');
+                // Feche o modal ou realize qualquer ação necessária após a atualização
+                props.onClose();
+            })
+            .catch((error) => {
+                console.error('Erro ao atualizar dados:', error);
+            });
+    
+            setIsEditMode(false);
+        };
+
         return(
-        <div className={styles.BookData}>
-        <img className={styles.BookCover} src="" />
+        <div className={`${styles.BookData} ${props.isEditMode ? styles.InEdition : ''}`}>
 
         <div className={styles.DataList}>
-            <img className={styles.CapeImg} src={props.cover} />
+            <div className={styles.CapeImg}><img src={props.cover} /></div>
             
             <div className={styles.DataLines}>
                 <div className={styles.TopDataList}>
-                <h1 className={styles.BookTitle}>{props.title}</h1>
+                <h1 className={`${styles.BookTitle} ${props.isEditMode ? styles.InEdition : ''}`}>{props.title}</h1>
 
                 <button className={styles.CloseBButton} onClick={props.onClose}>
                     <img src={closeIcon} />
@@ -72,98 +113,139 @@ function BookData(props){
 
                 <div className={styles.DataLine1}>
                     <div className={styles.BlockData}>
-                        <label htmlFor="id">Rótulo</label>
+                        <label htmlFor="id" className={`${props.isEditMode ? styles.InEdition : ''}`}>Rótulo</label>
                         <div id={styles.id} name="id">{props.id}</div>
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label htmlFor="gender">Gênero</label>
-                        <div id={styles.gender} name="gender">{props.gender}</div>
+                        <label htmlFor="genre" className={`${props.isEditMode ? styles.InEdition : ''}`}>Gênero</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.genre} className={styles.InEditionInput} name="genre" defaultValue={props.genre} value={editedValues.genre} onChange={(e) => setEditedValues({ ...editedValues, genre: e.target.value })}/>) 
+                        : ( <div id={styles.genre} name="genre">{props.genre}</div>)
+                        }
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label htmlFor="author">Autor</label>
-                        <div id={styles.author} name="author">{props.author}</div>
+                        <label htmlFor="author" className={`${props.isEditMode ? styles.InEdition : ''}`}>Autor</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.author} className={styles.InEditionInput} name="author" defaultValue={props.author} />) 
+                        : ( <div id={styles.author} name="author">{props.author}</div>)
+                        }
                     </div>
                 </div>
 
                 <div className={styles.DataLine2}>
                     <div className={styles.BlockData}>
-                        <label>Estado</label>
-                        <div id={styles.name} name="name">{props.name}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Estado</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.status} className={styles.InEditionInput} name="status" defaultValue={props.status} />) 
+                        : ( <div id={styles.status} name="status">{props.status}</div>)
+                        }
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label>Data do estado</label>
-                        <div id={styles.since} name="since">{props.since}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Data do estado</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.since} className={styles.InEditionInput} name="since" defaultValue={props.since} />) 
+                        : ( <div id={styles.since} name="since">{props.since}</div>)
+                        }
                     </div>
                     
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label>Exemplares</label>
-                        <div id={styles.copies} name="copies">{props.copies}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Exemplares</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.copies} className={styles.InEditionInput} name="copies" defaultValue={props.copies} />) 
+                        : ( <div id={styles.copies} name="copies">{props.copies}</div>)
+                        }
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label>Qtd. Geral</label>
-                        <div id={styles.quantity} name="quantity">{props.quantity}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Qtd. Geral</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.quantity} className={styles.InEditionInput} name="quantity" defaultValue={props.quantity} />) 
+                        : ( <div id={styles.quantity} name="quantity">{props.quantity}</div>)
+                        }
                     </div>
                 </div>
 
                 <div className={styles.DataLine3}>
                     <div className={styles.BlockData}>
-                        <label>Descrição do estado</label>
-                        <div id={styles.description} name="description">{props.description}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Descrição do estado</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.description} className={styles.InEditionInput} name="description" defaultValue={props.description} />) 
+                        : ( <div id={styles.description} name="description">{props.description}</div>)
+                        }
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label>Até</label>
-                        <div id={styles.until} name="until">{props.until}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Até</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.until} className={styles.InEditionInput} name="until" defaultValue={props.until} />) 
+                        : ( <div id={styles.until} name="until">{props.until}</div>)
+                        }
                     </div>
 
                     <hr />
 
                     <div className={styles.BlockData}>
-                        <label>Adquirido em</label>
-                        <div id={styles.registered} name="registered">{props.registered}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Adquirido em</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.registered} className={styles.InEditionInput} name="registered" defaultValue={props.registered} />) 
+                        : ( <div id={styles.registered} name="registered">{props.registered}</div>)
+                        }
                     </div>
                 </div>
                 
                 <div className={styles.DataLine4}>
                     <div className={styles.BlockData}>
-                        <label>Tipo de livro</label>
-                        <div id={styles.type} name="type">{props.type}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Tipo de livro</label>
+                        {props.isEditMode ? 
+                          ( <input type="text" id={styles.type} className={styles.InEditionInput} name="type" defaultValue={props.type} />) 
+                        : ( <div id={styles.type} name="type">{props.type}</div>)
+                        }
                     </div>
 
                     <hr style={{ margin: '19px 10px' }} />
 
                     <div className={styles.BlockData}>
-                        <label>Edição</label>
-                        <div id={styles.edition} name="edition">{props.edition}</div>
+                        <label className={`${props.isEditMode ? styles.InEdition : ''}`}>Edição</label>
+                                                {props.isEditMode ? 
+                          ( <input type="text" id={styles.edition} className={styles.InEditionInput} name="edition" defaultValue={props.edition} />) 
+                        : ( <div id={styles.edition} name="edition">{props.edition}</div>)
+                        }
+
+                        {props.isEditMode ? (
+                                <button className={styles.SaveBButton} onClick={handleSaveClick}>
+                                    <img src={saveIcon} />
+                                </button>
+                        ) : (
+                            <>
+                                <button className={styles.HistoryButton}>
+                                    <img src={historyIcon} />
+                                </button>
+                                <button className={styles.ToLoan}>Emprestar</button>
+                                <button className={styles.EditBButton} onClick={props.onEditClick}>
+                                    <img src={editIcon} />
+                                </button>
+                            </>
+                        )}
                     </div>
                 </div>
                 
                 <div className={styles.DataLine5}>
-                    <button className={styles.HistoryButton}>
-                        <img src={historyIcon} />
-                    </button>
 
-                    <button className={styles.ToLoan}>Emprestar</button>
-
-                    <button className={styles.EditBButton}>
-                    <img src={editIcon} />
-                    </button>
                 </div>
             </div>
         </div>
--- bibliodaurea/src/components/BooksList.js
+++ bibliodaurea/src/components/BooksList.js
@@ -13,28 +13,39 @@ import { getDatabase, ref, get, child } from 'firebase/database';
 const db = getDatabase(app);
 
 function BooksList(){
-    const [booksList, setBooksList] = useState([])
+    const [booksList, setBooksList] = useState([]);
     const [selectedBookId, setSelectedBookId] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar a visibilidade do pop-up
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
-        async function fetchData() {
-          try {
-            const booksRef = ref(db, 'books');
-            const bookSnapshot = await get(booksRef);
-          
-            if (bookSnapshot.exists()) {
-              const books = bookSnapshot.val();
-              setBooksList(books);
-            } else {
-              console.log('Sem livros.');
-            }
-          } catch (error) {
-            console.error('Erro ao buscar dados dos livros:', error);
+      let unsubscribe;
+  
+      async function fetchData() {
+        try {
+          const booksRef = ref(db, 'books');
+          const bookSnapshot = await get(booksRef);
+  
+          if (bookSnapshot.exists()) {
+            unsubscribe = onValue(booksRef, (snapshot) => {
+              const updatedData = snapshot.val();
+              setBooksList(updatedData);
+            });
+          } else {
+            console.log('Sem livros.');
           }
+        } catch (error) {
+          console.error('Erro ao buscar dados dos livros:', error);
         }
-        fetchData();
-      }, []);
+      }
+  
+      fetchData();
+  
+      return () => {
+        if (unsubscribe) {
+          unsubscribe();
+        }
+      };
+    }, []);
 
       function handleBookClick(bookId) {
         setSelectedBookId(bookId);
@@ -66,7 +77,7 @@ function BooksList(){
                             <th className={`${styles.ThBList} ${styles.CapeBList}`}>Capa</th>
                             <th className={`${styles.ThBList} ${styles.TitleBList}`}>Título</th>
                             <th className={`${styles.ThBList} ${styles.StatusBList}`}>Estado</th>
-                            <th className={`${styles.ThBList} ${styles.GenderBList}`}>Gênero</th>
+                            <th className={`${styles.ThBList} ${styles.GenreBList}`}>Gênero</th>
                             <th className={`${styles.ThBList} ${styles.DescriptionBList} ${styles.RightBorder}`}>Descrição do estado</th>
                         </tr>
                     </thead>
@@ -74,15 +85,15 @@ function BooksList(){
                         {booksList.map((book, index) => (
                             <tr key={index} onClick={() => handleBookClick(book.id)} className={`${index % 2 === 0 ? styles.LightLine : styles.DefaultLine}`}>
                             <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''} ${index === booksList.length - 1 ? styles.EndBAround : ''}`}> {book.id} </td>
-                            <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''}`}> <img width="60px" src={book.cover} alt={book.title} /> </td>
+                            <td className={`${styles.TdBList} ${styles.CoverSize} ${index % 2 === 0 ? styles.GreenFont : ''}`}> <img src={book.cover} alt={book.title} /> </td>
                             <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''}`}> {book.title} </td>
 
                             <td className={`${styles.TdBList}`}>
                                 <button className={book.status == "Disponível" ? styles.Avaliable : book.status == "Indisponível" ? styles.Unvaliable : book.status == "Bloqueado" ? styles.Blocked : book.status == "Extraviado" ? styles.Lost : book.status == "Emprestado" ? styles.Borrowed : "Status indefinido"}>{book.status}</button>
                             </td>
 
-                            <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''}`}> {book.gender} </td>
-                            <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''} ${index === booksList.length - 1 ? styles.EndBAround2 : ''}`}> {book.description} </td>
+                            <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''}`}> {book.genre.length > 50 ? `${book.genre.substring(0, 50)}...` : book.genre} </td>
+                            <td className={`${styles.TdBList} ${index % 2 === 0 ? styles.GreenFont : ''} ${index === booksList.length - 1 ? styles.EndBAround2 : ''}`}> {book.description.length > 50 ? `${book.description.substring(0, 50)}...` : book.description} </td>
                             </tr>
                         ))}
 
