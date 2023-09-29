ffe95b9328f00a4ead8f4f4a6cc2003777a5a286
Antoneli <antonelicavalcante@gmail.com>
2023-09-29 09:49:40 -0300

    FormBook

--- bibliodaurea/src/components/FormBook.js
+++ bibliodaurea/src/components/FormBook.js
@@ -9,6 +9,121 @@ import app from './../firebaseConfig/index.js';
 import { ref, push, getDatabase, set } from 'firebase/database'; // Importe as funções corretamente
 
 function FormBook(){
+  const [bookData, setBookData] = useState({
+    title: '',
+    genre: '',
+    author: '',
+    cover: '',
+    edition: '',
+    copies: 0,
+    id: '' // Adicione um campo "id" vazio para rastrear o ID gerado automaticamente
+  });
+
+  //CONST'S FILE
+  const fileInputRef = useRef(null);
+
+  const handleCustomButtonClick = () => {
+    // Aciona o clique no input de arquivo quando o botão personalizado é clicado
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (e) => {
+  const file = e.target.files[0];
+  //CODE'S FILE
+  if (file) {
+    var fileReader = new FileReader();
+    fileReader.onload = function (e) {
+      document.getElementById("photo").src = e.target.result;
+      let linkCapa = e.target.result;
+      console.log(linkCapa);
+      document.getElementById("linkImg").value = linkCapa;
+      // Oculta o input de arquivo após o carregamento da imagem
+      fileInputRef.current.style.display = 'none';
+    };
+    fileReader.readAsDataURL(file);
+    // Atualiza a visibilidade da imagem
+    document.getElementById("photo").style.opacity = '1';
+
+    console.log('Arquivo selecionado:', file.name);
+
+    setBookData({
+      ...bookData,
+      cover: file.name,
+    });
+    console.log(bookData)
+  }
+};
+
+  const setSelectedFile = (file) => {
+    setBookData({
+      ...bookData,
+      cover: file.name, // Atualize o campo "cover" com o arquivo selecionado
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const db = getDatabase(app);
+
+    try {
+      const booksRef = ref(db, 'books'); // Referência à coleção de livros
+
+      // Insira o novo livro no banco de dados
+      const newBookRef = push(booksRef);
+      const generatedId = newBookRef.key; // Obtenha o ID gerado automaticamente
+
+      // Atualize o objeto bookData com o ID gerado
+      setBookData({
+        ...bookData,
+        id: generatedId
+      });
+
+      // Insira os dados do livro, incluindo o ID, no banco de dados
+      await set(newBookRef, bookData);
+      console.log(bookData);
+
+      // Limpar o formulário após o registro bem-sucedido
+      setBookData({
+        title: '',
+        genre: '',
+        author: '',
+        cover: '',
+        edition: '',
+        copies: 0,
+        id: ''
+      });
+
+      console.log('Livro registrado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao registrar o livro:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+  
+    // Se o campo que está sendo alterado for o campo de arquivo (cover), 
+    // atualize o objeto bookData com o arquivo selecionado em vez de apenas o valor
+    console.log(name)
+    if (name === 'cover') {
+      const selectedFile = e.target.files[0];
+      console.log("TA NO COVER");
+      setBookData({
+        ...bookData,
+        [name]: selectedFile, // Use o arquivo selecionado em vez do valor
+      });
+    } else {
+      console.log("NAO TA NO COVER");
+      setBookData({
+        ...bookData,
+        [name]: value,
+      });
+    }
+  };
+  
+
     return (
       <div className={styles.container}>
         <form onSubmit={handleSubmit}>
@@ -32,10 +147,7 @@ function FormBook(){
               <input className={styles.inputValue1} type="text" name="title" value={bookData.title} onChange={handleInputChange} required/><br></br>
             </div>
 
-        <div className={styles.inputForm}>
-         <label>Autor</label>
-          <input className={styles.inputValue1} type="text" name="author"/><br></br>
-        </div>
+            <br></br>
 
             <div className={styles.inputForm}>
               <label>Gênero</label>
@@ -74,13 +186,12 @@ function FormBook(){
           </div>
       </form>
       </div>
-
-      
       
         )
 }
 
 
-export default FormBook;
 
+
+ export default FormBook
 
